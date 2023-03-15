f0e25490896bd6ab8151b047228b49b25c12c90a
jsdawn <kingwyh1993@163.com>
2023-03-15 11:48:02 +0800

    first commit

---
+++ App.js
@@ -0,0 +1,23 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>App</Text>
+    </View>
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    fontSize: 28,
+  },
+});
