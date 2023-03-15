2a35f8c6f87558a27acda92f21ac6ed332dcd0c0
jsdawn <kingwyh1993@163.com>
2023-03-15 15:18:11 +0800

    add react-navigation

--- App.js
+++ App.js
@@ -1,28 +1,38 @@
-import {StyleSheet, Text, View} from 'react-native';
 import React, {useEffect} from 'react';
+import {Text, View} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import SplashScreen from 'react-native-splash-screen';
 
-const App = () => {
+function HomeScreen() {
+  return (
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text>Home!</Text>
+    </View>
+  );
+}
+
+function SettingsScreen() {
+  return (
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text>Settings!</Text>
+    </View>
+  );
+}
+
+const Tab = createBottomTabNavigator();
+
+export default function App() {
   useEffect(() => {
     SplashScreen.hide();
   }, []);
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>App</Text>
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Settings" component={SettingsScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
-};
-
-export default App;
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text: {
-    fontSize: 28,
-  },
-});
+}
