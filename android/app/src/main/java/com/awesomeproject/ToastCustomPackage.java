2b5742f6e645bf805ca3468aabb76583c3b3b0db
jsdawn <kingwyh1993@163.com>
2023-03-16 18:37:48 +0800

    add Android module

---
+++ android/app/src/main/java/com/awesomeproject/ToastCustomPackage.java
@@ -0,0 +1,30 @@
+package com.awesomeproject;
+
+import com.facebook.react.ReactPackage;
+import com.facebook.react.bridge.NativeModule;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.uimanager.ViewManager;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class ToastCustomPackage implements ReactPackage {
+
+  @Override
+  public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
+    return Collections.emptyList();
+  }
+
+  @Override
+  public List<NativeModule> createNativeModules(
+      ReactApplicationContext reactContext) {
+    List<NativeModule> modules = new ArrayList<>();
+    
+    // 注册 ToastModule 模块
+    modules.add(new ToastModule(reactContext));
+
+    return modules;
+  }
+
+}
--- src/views/My/index.js
+++ src/views/My/index.js
@@ -1,10 +1,20 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, View, Button} from 'react-native';
 import React from 'react';
+import {NativeModules} from 'react-native';
 
 const My = () => {
   return (
     <View style={styles.container}>
       <Text style={styles.text}>旅行者，你将去往何方～</Text>
+      <Button
+        title="提示语"
+        onPress={() => {
+          NativeModules.ToastExample.show(
+            'Awesome222',
+            NativeModules.ToastExample.SHORT,
+          );
+        }}
+      />
     </View>
   );
 };
